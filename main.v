d8a9c7996e73c4e27761839efabaa8b4e52a5d01
ArdeshirV <e-job@protonmail.com>
2023-07-13 11:19:45 +0330

    Add evaluate_speed

--- main.v
+++ main.v
@@ -2,3 +2,15 @@
 fn main() {
     println("The V Programming Language")
 }
+
+fn evaluate_speed() {
+    start := v.time.now()
+
+    //some_algorithm()
+
+    end := v.time.now()
+    elapsed := end - start
+
+    println('Execution time:', elapsed)
+}
+

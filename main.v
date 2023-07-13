8b27ce46f50ec67397c65891db571ae6814f1bf3
ArdeshirV <e-job@protonmail.com>
2023-07-13 11:21:50 +0330

    Add selection_sort

--- main.v
+++ main.v
@@ -45,3 +45,16 @@ fn bubble_sort(mut arr []int) {
     }
 }
 
+fn selection_sort(mut arr []int) {
+    n := len(arr)
+    for i := 0; i < n-1; i++ {
+        min_idx := i
+        for j := i + 1; j < n; j++ {
+            if arr[j] < arr[min_idx] {
+                min_idx = j
+            }
+        }
+        arr.swap(i, min_idx)
+    }
+}
+

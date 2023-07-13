9077b17f5f1762a8f538e952205bb837cede6643
ArdeshirV <e-job@protonmail.com>
2023-07-13 11:22:56 +0330

    Add insertions_sort

--- main.v
+++ main.v
@@ -58,3 +58,16 @@ fn selection_sort(mut arr []int) {
     }
 }
 
+fn insertion_sort(mut arr []int) {
+    n := len(arr)
+    for i := 1; i < n; i++ {
+        key := arr[i]
+        j := i - 1
+        for j >= 0 && arr[j] > key {
+            arr[j+1] = arr[j]
+            j--
+        }
+        arr[j+1] = key
+    }
+}
+
